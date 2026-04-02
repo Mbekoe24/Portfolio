5879e1eb1e1d3e37e30ac7aa27a9508bb94ee12f
MBest24 <mbekoe24@gmail.com>
2026-04-02 02:04:15 -0400

    added new website to projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,6 +5,7 @@ import Nba from "../images/nbadatabase.png";
 import Poke from "../images/pokeapptest.png";
 import Recipe from "../images/recipebook.png";
 import Netflix from "../images/netflixclone.png";
+import PokeQuiz from "../images/pokequizrevamp.png";
 
 export const PROJECTS = [
   {
@@ -19,6 +20,16 @@ export const PROJECTS = [
   },
   {
     id: 2,
+    title: "Poké Quiz",
+    image: PokeQuiz,
+    imageAlt: "Poké Quiz",
+    description:
+      "A modern single-page Pokémon knowledge quiz application and a complete revamp of my original 2020 Poké App project, reflecting the evolution of my coding skills and overall technical comprehension. The app challenges users to identify species from silhouettes, full artwork, types, and cries, with customizable generations and difficulty levels. It includes high score tracking and a shareable, replayable run feature via URL. I leveraged AI agents such as Claude and Cursor to refactor legacy logic and implement new functionality. Built with React, TypeScript, Vite, and Tailwind CSS",
+    tech: ["Typescript", "React", "Vite", "ClaudeAI"],
+    link: "hhttps://poke-quiz.surge.sh/",
+  },
+  {
+    id: 3,
     title: "Anchoi Restaurant Website",
     image: Anchoi,
     imageAlt: "Anchoi Restaurant",
@@ -28,7 +39,7 @@ export const PROJECTS = [
     link: "https://an-choi.netlify.app/",
   },
   {
-    id: 3,
+    id: 4,
     title: "Instapic",
     image: Instapic,
     imageAlt: "Instapic",
@@ -38,17 +49,17 @@ export const PROJECTS = [
     link: "https://instapic.surge.sh/",
   },
   {
-    id: 4,
+    id: 5,
     title: "Poké App",
     image: Poke,
     imageAlt: "Poké App",
     description:
-      "Built a Pokémon trivia game using data from the PokéAPI that generates multiple choice questions where users guess \"Who's That Pokémon?\"",
+      'Built a Pokémon trivia game using data from the PokéAPI that generates multiple choice questions where users guess "Who\'s That Pokémon?"',
     tech: ["Vanilla JS", "HTML", "CSS"],
     link: "https://pokeapptest.surge.sh/",
   },
   {
-    id: 5,
+    id: 6,
     title: "NBA Database 2017-18",
     image: Nba,
     imageAlt: "NBA Database",
@@ -58,7 +69,7 @@ export const PROJECTS = [
     link: "https://github.com/Mbekoe24/NBADatabase",
   },
   {
-    id: 6,
+    id: 7,
     title: "Recipe Book",
     image: Recipe,
     imageAlt: "Recipe Book",
@@ -68,7 +79,7 @@ export const PROJECTS = [
     link: "https://my-recipebook.surge.sh/",
   },
   {
-    id: 7,
+    id: 8,
     title: "Temperature App",
     image: Weather,
     imageAlt: "Temperature App",
